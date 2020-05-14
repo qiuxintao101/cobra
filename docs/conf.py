482ec2438ee0f07074d3f5a2bcc0b12237dedc70
qiuxintao101 <qiuxintao@hotmail.com>
2020-05-14 15:00:51 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, CFCE'
 author = 'Xintao Qiu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.2'
 
 
 # -- General configuration ---------------------------------------------------
